94928894fabf906eb22fef2f9b3f284f82d404e0
ktae13-npnf <ktae13@gmail.com>
2017-08-16 23:14:19 -0700

    - custom script to handle symbol.

---
+++ AlohaShaveIce_v2/Assets/Script/Editor/CustomPostImporting.cs
@@ -0,0 +1,34 @@
+﻿using UnityEditor;
+using System.IO;
+
+public class CustomPostImporting : AssetPostprocessor 
+{
+
+	static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromAssetPaths)
+	{
+		SetScriptingDefineSymbols();
+	}
+
+	static void SetScriptingDefineSymbols()
+	{  //1.3
+
+		string defines = "";
+		if (Directory.Exists("Assets/3rdParty/GoogleMobileAds"))
+			defines = defines + "; GOOGLE_MOBILE_ADS";
+		if (Directory.Exists("Assets/3rdParty/Chartboost"))
+			defines = defines + "; CHARTBOOST_ADS";
+		if (Directory.Exists("Assets/3rdParty/FacebookSDK")) {
+			defines = defines + "; FACEBOOK";
+			if (Directory.Exists("Assets/3rdParty/PlayFabSDK"))
+				defines = defines + "; PLAYFAB";
+			if (Directory.Exists("Assets/3rdParty/GameSparks"))
+				defines = defines + "; GAMESPARKS";
+
+		}
+		if (Directory.Exists("Assets/Plugins/UnityPurchasing"))
+			defines = defines + "; UNITY_INAPPS";
+		PlayerSettings.SetScriptingDefineSymbolsForGroup(BuildTargetGroup.Android, defines);
+		PlayerSettings.SetScriptingDefineSymbolsForGroup(BuildTargetGroup.iOS, defines);
+		PlayerSettings.SetScriptingDefineSymbolsForGroup(BuildTargetGroup.WSA, defines);//1.3.3
+	}
+}

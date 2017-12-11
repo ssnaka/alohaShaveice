64616e386e7a9fb426a4edadc20cc0925158afd1
ktae13-npnf <ktae13@gmail.com>
2017-12-11 21:58:35 +0900

    iphone x safe area.

---
+++ AlohaShaveIce_v2/Assets/JuiceFresh/Scripts/GUI/IPhoneXSafeArea.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IPhoneXSafeArea : MonoBehaviour {
+	[SerializeField]
+	RectTransform topPanel;
+	[SerializeField]
+	RectTransform bottomanel;
+
+	void OnEnable ()
+	{
+		float screenRatio = (float)Screen.height / (float)Screen.width;
+
+		if(screenRatio >= 2.0f)
+		{
+			topPanel.anchoredPosition = new Vector2(topPanel.anchoredPosition.x, -44.0f);
+			bottomanel.anchoredPosition = new Vector2(bottomanel.anchoredPosition.x, 34.0f);
+		}
+	}
+}

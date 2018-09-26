af94a245c32405d29b7d99726f352e55ac8fc39f
ktae13-npnf <ktae13@gmail.com>
2018-09-25 21:16:57 -0700

    Privacy policy button.

---
+++ AlohaShaveIce_v2/Assets/JuiceFresh/Scripts/Custom/PrivacyButtonScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrivacyButtonScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+//	// Update is called once per frame
+//	void Update () {
+//		
+//	}
+
+    public void OnPrivacyButtonPressed ()
+    {
+        Application.OpenURL("https://docs.google.com/document/d/1t5vePkLeGI3mGN3H3p7oWshcogkDOBa2rrUy4qP-3O0/edit#heading=h.9gl0i71zlh3w");
+    }
+
+}
